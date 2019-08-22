9df7ca296217810a7f1180495de370c82fcd5e6c
iOLO <516502968@qq.com>
2019-08-22 16:34:14 +0800

    增加post参数

---
+++ spring-boot-demo-log-aop/src/main/java/com/xkcoding/log/aop/vo/ResultVO.java
@@ -0,0 +1,16 @@
+package com.xkcoding.log.aop.vo;
+
+import lombok.Data;
+
+/**
+ * //TODO 添加类/接口功能描述
+ *
+ * @author Fengxinxin
+ * @date 2019-08-21
+ */
+@Data
+public class ResultVO {
+  private Integer num;
+
+  private String str;
+}
